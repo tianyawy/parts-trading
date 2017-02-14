2b41647f074db9b10fbfee5a0fabbfd6c2a8147d
Zilong Chen <zlchen@thoughtworks.com>
2017-02-14 19:34:20 +0800

    Remove duplicated main function

--- src/main/java/hello/Example.java
+++ src/main/java/hello/Example.java
@@ -1,20 +1,13 @@
 package hello;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-@EnableAutoConfiguration
 public class Example {
 
     @RequestMapping("/")
     String home() {
         return "Hello World!";
-    }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Example.class, args);
     }
 }
