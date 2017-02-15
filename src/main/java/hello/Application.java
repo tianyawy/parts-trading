5e3de4cec3993718d87ea44af14ff66095ad197a
Zilong Chen <zlchen@thoughtworks.com>
2017-02-15 11:24:56 +0800

    Remove redudant Bean

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -14,16 +14,4 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-        };
-    }
-
 }
